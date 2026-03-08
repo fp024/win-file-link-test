a343da88a7101c73eeff3dbe33750df4c13e88ca
fp024 <aopenasus@naver.com>
2026-03-09 05:48:45 +0900

    실제 경로를 반환하는 함수로 정리

--- scripts/junction-test.ps1
+++ scripts/junction-test.ps1
@@ -5,35 +5,33 @@ Invoke-AsAdmin -ScriptPath $PSCommandPath -UseX86
 $arch = if ([Environment]::Is64BitProcess) { "x64 (64비트)" } else { "x86 (32비트)" }
 Write-Host "== PowerShell 프로세스: $arch ==" -ForegroundColor Cyan
 
+
+### 경로를 입력받아 실제 경로를 반환 함수 ###
 function Resolve-RealPath {
   param([string]$Path)
 
-  $item = (Get-Item ($Path)).Target
+  $item = (Get-Item $Path).Target
   if ($item -is [array] -and $item.Count -gt 0) {
-    "정션/심볼릭 링크 원본 경로: $($item[0])"
+    return $item[0]
   }
-  else {
-    "링크가 아닌 경로 (또는 빈 Target): $($Path)"
-  }
+  return $Path
 }
 
+
+#### 테스트 ###
 # 실제 경로 : 관리자 권한이 필요없는 보통의 실제 경로
 Write-Host "== 일반 폴더 경로 =="
-$m2path = "C:\Normal_Folder"
-Resolve-RealPath -Path $m2path
+Resolve-RealPath -Path "C:\Normal_Folder"
 
 # 시스템 프로필 경로:  관리자 권한이 필요한 실제 경로
 Write-Host "== 시스템 프로필 경로 =="
-$m2path = "C:\Windows\System32\config\systemprofile\.m2"
-Resolve-RealPath -Path $m2path
+Resolve-RealPath -Path "C:\Windows\System32\config\systemprofile\.m2"
 
 # 정션 경로
 Write-Host "== 정션 링크 =="
-$m2path = "C:\Maven-Junction"
-Resolve-RealPath -Path $m2path
+Resolve-RealPath -Path "C:\Maven-Junction"
 
 # 심볼릭 링크 경로
 Write-Host "== 심볼릭 링크 =="
-$m2path = "C:\Maven-Symlink"
-Resolve-RealPath -Path $m2path
+Resolve-RealPath -Path "C:\Maven-Symlink"
 
